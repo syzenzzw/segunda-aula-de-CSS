582dd7857cffee7162f1cbad5ea201836fb05f5f
syzenzzw <162196005+syzenzzw@users.noreply.github.com>
2024-05-09 13:27:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const imgs = document.getElementByid('img');
+let count=1;
+document.getelementById("radio1").checked = true;
+
+setInterval(function(){
+
+}, 2000)
+
+function netImage(){
+	count++;
+	if (count>4) {
+		count = 1;
+
+	}
+
+	document.getelementById("radio1").checked = true;
+}
